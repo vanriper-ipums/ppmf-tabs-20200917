3410481b8819ae820be4ab671ed0243edea90110
David Van Riper <vanriper@umn.edu>
2020-09-18 17:08:39 +0000

    Added in handling for HU file from data release.

--- r_docs/00_load_ppmf_hu.r
+++ r_docs/00_load_ppmf_hu.r
@@ -1,11 +1,16 @@
-# 00_load_ppmf_hu.r
+# 00_load_and_prepare_ppmf_hu.r
 # Author: David Van Riper
 # Created: 2020-09-17
 # 
-# This script loads the ppmf housing unit microdata file into a data.table. The initial version of this script was 
+# This script loads the ppmf housing unit microdata file into a data.table. It changes the column headers to match the 2010 NHGIS values. 
+# It also sums the occupied and vacant counts to generate the total HU count. The initial version of this script was 
 # written to support the data released on 2020-09-17. 
 # 
-
+# The new column headers will be:
+# 
+# IFE001 - total housing units
+# IFE002 - occupied units
+# IFE003 - vacant units
 
 require(data.table)
 
@@ -13,5 +18,16 @@ require(data.table)
 # ppmf column types
 ppmf_col_classes <- c("character", "character", "character", "character", "character", "character", "integer", "integer")
 
+cols_h3 <- c("TABBLKST", "TABBLKCOU", "TABTRACTCE", "TABBLK", "IFE001", "IFE002", "IFE003")
+
 #### Load ppmf to a dt #### 
 dt_u <- fread("data/ppmf_20200917u.csv.gz", sep = ",", colClasses = ppmf_col_classes)
+
+#### Rename columns to correct values #### 
+setnames(dt_u, old = c("OCCUPIED_COUNT", "VACANT_COUNT"), new = c("IFE002_dp", "IFE003_dp"))
+
+#### Compute total housing units #### 
+dt_u[, IFE001_dp := IFE002_dp + IFE003_dp]
+
+#### Drop extra columns and then set column order #### 
+dt_u[, `:=` (VINTAGE = NULL, TABBLKGRPCE = NULL)] 
